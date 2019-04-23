e0ea240e4b77f46e73f3df22a249a108aeb4fc77
davidauza.engineer <davidauza01@gmail.com>
2019-04-23 09:11:42 -0500

    GridView experimenting.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Create a list of words
+        ArrayList<String> words = new ArrayList<>();
+        words.add(getString(R.string.numbers_one));
+        words.add(getString(R.string.numbers_two));
+        words.add(getString(R.string.numbers_three));
+        words.add(getString(R.string.numbers_four));
+        words.add(getString(R.string.numbers_five));
+        words.add(getString(R.string.numbers_six));
+        words.add(getString(R.string.numbers_seven));
+        words.add(getString(R.string.numbers_eight));
+        words.add(getString(R.string.numbers_nine));
+        words.add(getString(R.string.numbers_ten));
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        GridView listView = findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
     }
 }
